e8892fc4b0f1352500ecba6cad284a4d2974c000
yaknow23 <git@obelex.com>
2013-09-09 16:15:01 -0400

    New change

--- PROJ1/Global.asax.cs
+++ PROJ1/Global.asax.cs
@@ -14,7 +14,7 @@ namespace PROJ1 {
     public class MvcApplication : System.Web.HttpApplication {
         protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
-
+            //HI!
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
